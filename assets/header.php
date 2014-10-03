c9ba4491d031e09f61032d2632346bbabb0415fc
Luke Brown <me@luke.sx>
2014-10-03 13:17:34 +0100

    Initial Commit

---
+++ assets/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Luke Brown Photograpghy</title>
+
+<!-- ////////////////////////////////////
+	HEADER LINKS
+///////////////////////////////////// -->
+
+	<meta name="description" content="A2 Photograpghy Course Work By Luke Brown">
+	<meta name="author" content="Luke Brown">
+	<meta charset="utf-8">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic,300italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="./assets/css/bootstrap.css">
+	<link rel="stylesheet" href="./assets/css/style.css">
+
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/img/camera.ico">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/img/camera.ico">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/img/camera.ico">
+	<link rel="apple-touch-icon-precomposed" href="assets/img/camera.ico">
+	<link rel="icon" href="assets/img/camera.ico">
+
+</head>
+
+
+<!-- ////////////////////////////////////
+	BEGIN BODY
+///////////////////////////////////// -->
+
+<body>
---
+++ assets/navbar.php
@@ -0,0 +1,21 @@
+
+	<div id="top-nav" class="navbar navbar-fixed-top navbar-default">
+		<div class="container">
+			<div class="container">
+				<button type="button" class="btn btn-navbar" style="visibility: hidden;" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="collapse navbar-collapse">
+					<ul class="nav pull-right navbar-nav">
+						<li><a href="./">Home</a></li>
+						<li><a href="macro">Macro</a></li>
+						<li><a href="light&amp;focus">Light &amp; focus</a></li>
+						<li><a href="realities">Realities</a></li>
+						<li><a href="Overview" class="download" data-spy="affix" data-offset-top="450">Overview</a></li>
+					</ul>
+				</div>
+			</div><!--CONTAINER-->
+		</div><!--CONTAINER AGAIN-->
+	</div><!--NAVBAR-->
