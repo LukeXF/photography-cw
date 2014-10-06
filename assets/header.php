31ae2c3f878ed2855545ad0d79733d4887adfbb4
Luke Brown <me@luke.sx>
2014-10-06 03:14:51 +0100

    Splash Page

--- assets/header.php
+++ assets/header.php
@@ -1,33 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-	<title>Luke Brown Photograpghy</title>
-
-<!-- ////////////////////////////////////
-	HEADER LINKS
-///////////////////////////////////// -->
-
-	<meta name="description" content="A2 Photograpghy Course Work By Luke Brown">
-	<meta name="author" content="Luke Brown">
-	<meta charset="utf-8">
-
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic,300italic' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="./assets/css/bootstrap.css">
-	<link rel="stylesheet" href="./assets/css/style.css">
-
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/img/camera.ico">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/img/camera.ico">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/img/camera.ico">
-	<link rel="apple-touch-icon-precomposed" href="assets/img/camera.ico">
-	<link rel="icon" href="assets/img/camera.ico">
-
-</head>
+<?php
 
 
-<!-- ////////////////////////////////////
-	BEGIN BODY
-///////////////////////////////////// -->
+    // Sets the values for the navbar
+    $navbar = array(
+        "home" =>   array(
+            "active" => "",
+            "url" => "//joemethven.com",
+            "submenu" => array()
+        ),
 
-<body>
+        "portfolio" =>   array(
+            "active" => "",
+            "url" => "",          
+            "submenu" => array()
+        ),
+
+        "about" =>  array(
+            "active" => "",
+            "url" => "",          
+            "submenu" => array()
+        ),
+
+        "contact"=>array(
+            "active" => "",
+            "url" => "",          
+            "submenu" => array()
+        )
+
+    )
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="  ">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Luke Brown Photography</title>   
+
+        <link rel="stylesheet" href="assets/css/base.css">
+
+    </head>
+    <body class="dark-sidebar">
---
+++ index2.php
@@ -0,0 +1,139 @@
+<?php
+	include 'assets/header.php';
+	$activeTab = 'home';
+	include 'assets/navbar.php';
+?>
+
+		<!-- Page main wrapper -->	
+		<div id="main-content" class="abs dark-template">
+			<div class="page-wrapper">
+
+				<!-- Page main content -->	
+				<div class="page-main set-height-mobile no-side">
+
+					<!-- Kenburn slider -->	
+					<div class="kb-slider">
+					
+						<!-- Kenburn slide -->		
+						<div class="item">
+							<img src="assets/img/photos/docks.png" alt="img">
+						</div>
+						<!-- /Kenburn slide -->
+				
+						<div class="item">
+							<img src="assets/img/photos/grill.png" alt="img">
+						</div>
+						<!-- /Kenburn slide -->
+
+						<!-- Kenburn slide -->	
+						<div class="item">
+							<img src="assets/img/photos/fire.png" alt="img">
+						</div>
+						<!-- /Kenburn slide -->
+
+						<!-- Kenburn slide -->	
+						<div class="item">
+							<img src="assets/img/photos/stairs.png" alt="img">
+						</div>
+						<!-- /Kenburn slide -->
+
+						<!-- Kenburn slide -->	
+						<div class="item">
+							<img src="assets/img/photos/cpark.png" alt="img">
+						</div>
+						<!-- /Kenburn slide -->
+
+						<!-- Kenburn slide -->	
+						<div class="item">
+							<img src="assets/img/photos/liquid.png" alt="img">
+						</div>
+						<!-- /Kenburn slide -->
+
+						<!-- Kenburn slide -->	
+						<div class="item">
+							<img src="assets/img/photos/leaves.png" alt="img">
+						</div>
+						<!-- /Kenburn slide -->
+					</div>
+					<!-- /Kenburn slider -->	
+					
+					<!-- Caption -->	
+					<div class="caption cap-full">
+						<div class="vcenter-wrapper">
+							<div class="cap-lg cap-lg-def vcenter">
+								<h2 class="cap-title allcaps">Luke Brown<br/>Photography</h2>
+								<div class="cap-des">
+									<p>
+										The creative environment belonging to two years worth of photography coursework for digital viewing. 
+									</p>
+
+								</div>
+								<a href="#" class="btn btn-transparent btn-lg">Browse my work <i class="fa fa-long-arrow-right"></i></a>
+							</div>
+						</div>
+					</div>
+					<!-- /Caption -->	
+
+				</div>
+				<!-- /Page main content -->	
+
+					
+			</div>
+						
+		</div>
+		<!-- /Page main wrapper -->	
+
+		<!-- JavaScript files -->	
+
+		<!-- jquery core -->
+		<script type="text/javascript" src="assets/js/vendors/jquery-1.11.0.min.js"></script>
+
+		<!-- imagesLoaded jquery plugin -->
+		<script type="text/javascript" src="assets/js/vendors/imagesloaded.pkgd.min.js"></script>
+		
+		<!-- jquery isotop plugin -->
+		<script type="text/javascript" src="assets/js/vendors/isotope.pkgd.min.js"></script>
+
+		<!-- jquery history neede for ajax pages -->
+		<script type="text/javascript" src="assets/js/vendors/jquery.history.js"></script>
+
+		<!-- owwwlab jquery kenburn slider plugin -->
+		<script type="text/javascript" src="assets/js/jquery.owwwlab-kenburns.js"></script>
+
+		<!-- owwwlab jquery double carousel plugin -->
+		<script type="text/javascript" src="assets/js/jquery.owwwlab-DoubleCarousel.js"></script>
+
+		<!-- owwwwlab jquery video background plugin -->
+		<script type="text/javascript" src="assets/js/jquery.owwwlab-video.js"></script>
+
+		<!-- tweenmax animation framework -->
+		<script type="text/javascript" src="assets/js/vendors/TweenMax.min.js"></script>
+
+		<!-- jquery nice scroll plugin needed for vertical portfolio page -->
+		<script type="text/javascript" src="assets/js/vendors/jquery.nicescroll.min.js"></script>
+
+		<!-- jquery magnific popup needed for ligh-boxes -->
+		<script type="text/javascript" src="assets/js/vendors/jquery.magnific-popup.js"></script>
+
+		<!-- html5 media player -->
+		<script type="text/javascript" src="assets/js/vendors/mediaelement-and-player.min.js"></script>
+
+		<!-- jquery inview plugin -->
+		<script type="text/javascript" src="assets/js/vendors/jquery.inview.min.js"></script>
+
+		<!-- smooth scroll -->
+		<script type="text/javascript" src="assets/js/vendors/smoothscroll.js"></script>
+
+		
+
+		<!-- Master Slider -->
+		<script src="assets/masterslider/jquery.easing.min.js"></script>
+		<script src="assets/masterslider/masterslider.min.js"></script>
+		
+		<!-- theme custom scripts -->
+		<script type="text/javascript" src="assets/js/custom.js"></script>
+
+		<!-- /JavaScript files -->	
+		
+    </body>
+</html>
